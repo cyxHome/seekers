d2ccece55df3525fb33591cba27e3c154e42db0b
Yuxin Cao <cyx32524@gmail.com>
2015-02-28 14:30:21 -0800

    render page cookie test 2

--- routes/logined-index.js
+++ routes/logined-index.js
@@ -3,21 +3,21 @@ var data = require('../data.json');
 exports.view = function(req, res){
 	console.log(req.cookies.pageVersion);
 	if (req.cookies.pageVersion === 1)
-		res.render('index', data);
+		res.render('logined-index', data);
 	else if (req.cookies.pageVersion === 2)
-		res.render('index-alternative',data);
+		res.render('logined-index-alternative',data);
 	else 
 	{
 		var layoutSelector = Math.random();
 		if (layoutSelector < 0.5)
 		{
 			res.cookie('pageVersion', 1);
-			res.render('index', data);
+			res.render('logined-index', data);
 		}
 		else 
 		{
 			res.cookie('pageVersion', 2);
-			res.render('index-alternative',data);
+			res.render('logined-index-alternative',data);
 		}
 	}
 };
